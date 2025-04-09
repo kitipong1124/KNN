a720fd7ab40b2c8e441cebc9acd34e69b4fa4fa1
kitipong1124 <naktub.1124@gmail.com>
2025-04-09 12:06:46 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -22,7 +22,7 @@ with col3:
 
 html_7 = """
 <div style="background-color:#fec8ff;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h3>ข้อมูล iris หรือข้อมูลดอกไม้สำหรับทำนาย</h3></center>
+<center><h3 style="color:black;">ข้อมูล iris หรือข้อมูลดอกไม้สำหรับทำนาย</h3></center>
 </div>
 """
 st.markdown(html_7, unsafe_allow_html=True)
